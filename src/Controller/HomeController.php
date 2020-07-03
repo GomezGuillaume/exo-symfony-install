1509bc75ea8be7cf29cb6204f005fea32fba5a85
GOMEZ Guillaume <guillaume.gomez@lapiscine.pro>
2020-07-03 14:31:08 +0200

    Home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController {
 
+
     /**
      * @Route("/", name="home")
      */
-
     public function home() {
-        var_dump("hello home"); die;
+        var_dump("Bienvenue sur la page d'accueil"); die;
 
     }
 
+
+    /**
+     * @Route("/", name="test")
+     */
+    public function test() {
+        var_dump("Bienvenue sur la page test"); die;
+    }
+
+
+    /**
+     * @Route("/", name="article")
+     */
+    public function article() {
+        var_dump("Bienvenue sur la page article");
+    }
+
+    
+    /**
+     * @Route("/", name="contact")
+     */
+    public function contact() {
+        var_dump("Bienvenue sur la page contact");
+    }
+
+
 }
+
